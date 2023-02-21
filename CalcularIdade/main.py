ffc2abfa21bdfc43a7e61a32a843d890e100f0dc
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-21 17:37:28 -0400

    Projeto completo

--- CalcularIdade/main.py
+++ CalcularIdade/main.py
@@ -1,17 +1,19 @@
 # Calcular a Idade com base na data atual e a data de nascimento, ano, mes, dia.
+import datetime as dt
 
 def calcularIdade(ano, mes, dia):
-
-    import datetime as dt
 
     dataAtual = dt.datetime.now().date()
 
     dataNascimento = dt.date(ano,mes,dia)
 
-
+    calculoDaIdade = int((dataAtual-dataNascimento).days / 365.25)
 
     print(f'Data de Nascimento, {dataNascimento}')
     print(f'Data Atual, {dataAtual}')
+    print(f'Sua idade é: {calculoDaIdade} anos')
 
 if __name__ == '__main__':
-    calcularIdade(2000,10,2)
+    calcularIdade(1965,12,2)
+
+
