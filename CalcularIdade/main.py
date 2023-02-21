878fc1aa3b489caebbcfbebb36f874ceafe30e33
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-21 17:26:14 -0400

    Método CalcularIdade

--- CalcularIdade/main.py
+++ CalcularIdade/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# Calcular a Idade com base na data atual e a data de nascimento, ano, mes, dia.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def calcularIdade(ano, mes, dia):
 
+    print(f'Data de Nascimento, Dia {dia}, Mês {mes}, Ano {ano}')
+    print(f'Data de Nascimento, {dia}/{mes}/{ano}')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    calcularIdade(2000,10,2)
