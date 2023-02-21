dd63aea6c7dfb94fe6193ba534c154d22c2959e5
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-21 19:12:48 -0400

    Criação do método imprimir

--- CalcularIdade/main.py
+++ CalcularIdade/main.py
@@ -9,9 +9,15 @@ def calcularIdade(ano, mes, dia):
 
     calculoDaIdade = int((dataAtual-dataNascimento).days / 365.25)
 
+    imprimir(calculoDaIdade, dataAtual, dataNascimento)
+
+
+def imprimir(calculoDaIdade, dataAtual, dataNascimento):
+
     print(f'Data de Nascimento, {dataNascimento}')
     print(f'Data Atual, {dataAtual}')
     print(f'Sua idade é: {calculoDaIdade} anos')
+
 
 if __name__ == '__main__':
     calcularIdade(1965,12,2)
